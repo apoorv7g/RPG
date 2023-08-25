51b1007f411862da2397feca89a7b9f118c59891
APOORV7G <130035517+APOORV7G@users.noreply.github.com>
2023-08-25 20:30:16 +0530

    no_work_done_cause_space_doesnt_work;

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -4,9 +4,10 @@ import java.awt.image.BufferedImage;
 
 public class Entity 
 {
-	public int x,y;
+	public int worldx,worldy;
 	public int speed;
 	public BufferedImage up,down,left,right;
 	public String direction;
 	
 }
+						
